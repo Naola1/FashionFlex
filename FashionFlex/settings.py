cbf20238f3e0b65af965f95b321226755029b05a
naola1 <naolmitiku@gmail.com>
2024-10-30 14:29:03 +0300

    updated setting.py

--- FashionFlex/settings.py
+++ FashionFlex/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External Libraries
     "rest_framework",
+    "django_filters",
     # my apps
     "shop",
 ]
@@ -117,3 +118,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+                    'DEFAULT_FILTER_BACKENDS':
+                    ['django_filters.rest_framework.DjangoFilterBackend']
+                }
